18182ed81a3bfa5273c5db39ae53c94ed46820ed
italopy-dev <teixeirafernandesitalo1@gmail.com>
2026-05-24 10:32:37 -0300

    Organizando configurações

---
+++ settings.py
@@ -0,0 +1,45 @@
+documentos = [".txt", ".pdf", ".doc", ".docx", ".rtf"]
+compactados = [".zip", ".rar", ".7z", ".tar"]
+audios = [".mp3", ".wav", ".ogg", ".flac", ".m4a"]
+planilhas = [".xls", ".xlsx", ".csv"]
+imagens = [".png", ".jpg", ".jpeg", ".gif", ".webp", ".bmp", ".svg"]
+videos = [".mp4", ".mkv", ".avi", ".mov", ".webm"]
+apresentacoes = [".ppt", ".pptx", ".pps", ".ppsx", ".odp"]
+executaveis = [".exe", ".msi", ".bat"]
+ebooks = [".epub", ".mobi", ".azw3"]
+jogos = [".iso", ".rom", ".sav"]
+design = [".psd", ".ai"]
+codigos = [".py", ".java", ".html", ".css", ".js", ".cpp"]
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+    
+    
+
+        
+    
+    
+
+
+    
+
+
+
+
+
+
